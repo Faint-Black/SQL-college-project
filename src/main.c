9737de35827f783f69b310ee1a9a8e3a4cf7cf27
Cezar <fntblk@gmail.com>
2025-11-21 11:23:28 -0300

    SQL API package detection

--- src/main.c
+++ src/main.c
@@ -1,14 +1,34 @@
 #include <config.h>
 #include <gtk/gtk.h>
 
+#if defined(USE_MARIADB)
+#include <mariadb/mysql.h>
+#elif defined(USE_MYSQL)
+#include <mysql.h>
+#else
+#error "no DB API found"
+#endif
+
+static void
+hello_world_print (GtkWidget *widget, gpointer user_data)
+{
+  g_print ("Hello World!\n");
+}
+
 static void
 activate (GtkApplication *app, gpointer user_data)
 {
   GtkWidget *window;
+  GtkWidget *button;
 
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_window_set_title (GTK_WINDOW (window), "College Manager");
+  gtk_window_set_default_size (GTK_WINDOW (window), 500, 300);
+
+  button = gtk_button_new_with_label ("Hello SQL Manager");
+  g_signal_connect (button, "clicked", G_CALLBACK (hello_world_print), NULL);
+  gtk_window_set_child (GTK_WINDOW (window), button);
+
   gtk_window_present (GTK_WINDOW (window));
 }
 
@@ -18,7 +38,8 @@ main (int argc, char **argv)
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  app = gtk_application_new ("org.gtk.collegemanager",
+                             G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
