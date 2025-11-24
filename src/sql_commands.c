3fb65ceb1cff69d1ad1ef033f9b4d4b92d77676f
Cezar <fntblk@gmail.com>
2025-11-23 22:15:32 -0300

    enter SQL

---
+++ src/sql_commands.c
@@ -0,0 +1,14 @@
+#include "sql_commands.h"
+
+const char *sql_com_init[] = {
+  // let there be light!
+  "CREATE DATABASE IF NOT EXISTS college_manager",
+  // put light in a bulb
+  "USE college_manager",
+  // superclass; refer to docs/diagram
+  "CREATE TABLE scholars (\n"
+  "  id INT AUTO_INCREMENT PRIMARY KEY,\n"
+  "  full_name VARCHAR(256) NOT NULL\n"
+  ")",
+};
+const int sql_com_init_len = sizeof (sql_com_init) / sizeof (sql_com_init[0]);
---
+++ src/sql_commands.h
@@ -0,0 +1,4 @@
+#pragma once
+
+extern const char *sql_com_init[];
+extern const int sql_com_init_len;
