0b46991119edd76dd23bd3598146e40824c306ed
Cezar <fntblk@gmail.com>
2025-12-04 08:49:58 -0300

    log panel

--- src/main.c
+++ src/main.c
@@ -41,19 +41,30 @@ activate (GtkApplication *app, [[maybe_unused]] gpointer user_data)
   gtk_window_set_title (GTK_WINDOW (window), "College Manager");
   gtk_window_set_default_size (GTK_WINDOW (window), 500, 300);
 
-  GtkWidget *paned = gtk_paned_new (GTK_ORIENTATION_HORIZONTAL);
+  GtkWidget *h_paned = gtk_paned_new (GTK_ORIENTATION_HORIZONTAL);
+  GtkWidget *v_paned = gtk_paned_new (GTK_ORIENTATION_VERTICAL);
 
-  GtkWidget *left_box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
-  gtk_widget_set_margin_top (left_box, 6);
-  gtk_widget_set_margin_bottom (left_box, 6);
-  gtk_widget_set_margin_start (left_box, 6);
-  gtk_widget_set_margin_end (left_box, 6);
+  GtkWidget *top_box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
+  gtk_widget_set_margin_top (top_box, 6);
+  gtk_widget_set_margin_bottom (top_box, 6);
+  gtk_widget_set_margin_start (top_box, 6);
+  gtk_widget_set_margin_end (top_box, 6);
+
+  GtkWidget *bottom_box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
+  gtk_widget_set_margin_top (bottom_box, 6);
+  gtk_widget_set_margin_bottom (bottom_box, 6);
+  gtk_widget_set_margin_start (bottom_box, 6);
+  gtk_widget_set_margin_end (bottom_box, 6);
 
   GtkWidget *right_box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
   gtk_widget_set_margin_top (right_box, 6);
   gtk_widget_set_margin_bottom (right_box, 6);
   gtk_widget_set_margin_start (right_box, 6);
   gtk_widget_set_margin_end (right_box, 6);
+
+  GtkWidget *log_textbox = gtk_text_view_new ();
+  gtk_text_view_set_editable (GTK_TEXT_VIEW (log_textbox), FALSE);
+  gtk_text_view_set_monospace (GTK_TEXT_VIEW (log_textbox), TRUE);
 
   GtkWidget *create_button
       = gtk_button_new_with_label ("Initialize SQL database");
@@ -65,24 +76,33 @@ activate (GtkApplication *app, [[maybe_unused]] gpointer user_data)
                     G_CALLBACK (callback_drop_database), NULL);
 
   GtkWidget *placeholder_label = gtk_label_new ("placeholder");
+  GtkWidget *log_label = gtk_label_new ("Log output:");
 
   // set parent/child model:
   //
   // APP_WINDOW
-  // L PANED
-  //   L LEFT_BOX
-  //   | L DB_CREATION_BUTTON
-  //   | L DB_DESTRUCTION_BUTTON
+  // L HORIZONTAL_PANED
+  //   L VERTICAL_PANED
+  //   | L TOP_BOX
+  //   | | L DB_CREATION_BUTTON
+  //   | | L DB_DESTRUCTION_BUTTON
+  //   | L BOTTOM_BOX
+  //   |   L LOG_OUTPUT_LABEL
+  //   |   L LOG_OUTPUT_TEXTBOX
   //   L RIGHT_BOX
   //     L PLACEHOLDER_LABEL
-  gtk_window_set_child (GTK_WINDOW (window), paned);
-  gtk_paned_set_start_child (GTK_PANED (paned), left_box);
-  gtk_paned_set_end_child (GTK_PANED (paned), right_box);
-  gtk_box_append (GTK_BOX (left_box), create_button);
-  gtk_box_append (GTK_BOX (left_box), destroy_button);
+  gtk_window_set_child (GTK_WINDOW (window), h_paned);
+  gtk_paned_set_start_child (GTK_PANED (h_paned), v_paned);
+  gtk_paned_set_start_child (GTK_PANED (v_paned), top_box);
+  gtk_box_append (GTK_BOX (top_box), create_button);
+  gtk_box_append (GTK_BOX (top_box), destroy_button);
+  gtk_paned_set_end_child (GTK_PANED (v_paned), bottom_box);
+  gtk_box_append (GTK_BOX (bottom_box), log_label);
+  gtk_box_append (GTK_BOX (bottom_box), log_textbox);
+  gtk_paned_set_end_child (GTK_PANED (h_paned), right_box);
   gtk_box_append (GTK_BOX (right_box), placeholder_label);
 
-  // render app window and all children
+  // set master widget
   gtk_window_present (GTK_WINDOW (window));
 }
 
